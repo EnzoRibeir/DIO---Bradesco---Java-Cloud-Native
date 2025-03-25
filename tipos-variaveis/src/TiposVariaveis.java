3fe0534ad44d92a090ba68e715d8add287a44873
Enzo Ribeiro <enzodesenveloper@gmail.com>
2025-03-25 13:50:23 -0300

    explorando tipos primitivos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Tipos primitivos
+
+        boolean verdadeiro = true;
+        
+        byte idade = 123;
+		short ano = 2021;
+		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+		long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+		float pi = 3.14F;
+		double salario = 1275.33;
+
+        String meuNome = "Fulano de Tal";
+
+        final double PI = 3.14159265358979323846;
     }
 }
